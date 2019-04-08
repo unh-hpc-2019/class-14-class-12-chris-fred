5cb95f7af7ecd240acb302e7b8ec88c20966c055
crswong888 <crswong888@gmail.com>
2019-04-08 13:59:03 -0400

    Declared thread_id and n_threads outside of the parallel block to experiment with what happens.

--- hello/hello_openmp.c
+++ hello/hello_openmp.c
@@ -7,10 +7,11 @@ main(int argc, char **argv)
   // single-threaded
   printf("Hi, just starting.\n");
   // start running multiple threads
+
+  int thread_id = omp_get_thread_num();
+  int n_threads = omp_get_num_threads();
 #pragma omp parallel
  {
-    int thread_id = omp_get_thread_num();
-    int n_threads = omp_get_num_threads();
     printf("Hello %d/%d\n", thread_id, n_threads);
   }
   // back to single-threaded
